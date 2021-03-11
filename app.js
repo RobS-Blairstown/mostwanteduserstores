e0009620ea807b52325f92b15e51453f2c0aea34
Rob <rob.sutton123@gmail.com>
2021-03-11 16:42:19 -0500

    working on fixing the table to make it more efficient and to show actual elements that are being passed into the search bar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 function searchByName(){
     // Grabbing the values from our nameForm form and inputs.
     let firstNameInput = document.forms['nameForm']['fname'].value;
+    console.log(firstNameInput);
     let lastNameInput = document.forms['nameForm']['lname'].value;
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
@@ -12,12 +13,14 @@ function searchByName(){
         }
         return false;
     });
-    
+    console.log(filteredPeople[0]);
     // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
-        console.log(filteredPeople);
+        
     }else{
-        console.log('Sorry, looks like there is no one with that name.');
+        // while(filteredPeople === false){
+        //     callback(searchByName);
+        // }
     }
 }
 
