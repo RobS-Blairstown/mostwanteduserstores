690ca3342a87bcc7caa314f2a031f3ff55359c0b
Rob <rob.sutton123@gmail.com>
2021-03-12 13:28:57 -0500

    Finished creating seperate form id's for each individual search criteria. Need to make functions for each single search criteria as well

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 function searchByName(){
     // Grabbing the values from our nameForm form and inputs.
     let firstNameInput = document.forms['nameForm']['fname'].value;
-    console.log(firstNameInput);
     let lastNameInput = document.forms['nameForm']['lname'].value;
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
@@ -13,14 +12,40 @@ function searchByName(){
         }
         return false;
     });
-    console.log(filteredPeople[0]);
     // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
-        
+        console.log(filteredPeople);
     }else{
-        // while(filteredPeople === false){
-        //     callback(searchByName);
-        // }
+       console.log("There is nobody with this name.");
     }
+}
+
+function searchById(){
+    let idInput = document.forms['idForm']['id'].value;
+
+    let filteredPeople = people.filter(function (person) {
+        if (person.id === idInput){
+        return true;
+        }
+        return false;
+    });
+
+    if(filteredPeople.length > 0){
+        console.log(filteredPeople);
+    }else{
+        console.log("There is no one with this ID Number.");
+    }
+}
+
+function searchGender(){
+    let genderInput = document.forms['genderForm']['gender'].value;
+
+    let filteredPeople = people.filter(function (person) {
+        if (person.id === genderInput){
+        return true;
+        }
+        return false;
+    });
+    console.log(filteredPeople);
 }
 
